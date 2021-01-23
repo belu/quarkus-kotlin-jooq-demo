4c243293e3563b5f3c196a146a5c382c95192d99
Bernhard Lutzmann <bernhard.lutzmann@gmail.com>
2021-01-23 21:21:21 +0100

    Improve REST responses

--- src/main/kotlin/me/belu/rest/AuthorsResource.kt
+++ src/main/kotlin/me/belu/rest/AuthorsResource.kt
@@ -8,6 +8,7 @@ import javax.ws.rs.*
 import javax.ws.rs.core.Context
 import javax.ws.rs.core.MediaType
 import javax.ws.rs.core.Response
+import javax.ws.rs.core.Response.Status.BAD_REQUEST
 import javax.ws.rs.core.UriInfo
 
 @Path("authors")
@@ -23,13 +24,14 @@ class AuthorsResource {
   fun addAuthor(@QueryParam("firstName") firstName: String, @QueryParam("lastName") lastName: String): Response {
     val author = service.addAuthor(firstName, lastName)
 
-    return Response.ok().location(locationFor(author)).build()
+    return Response.ok(author).location(locationFor(author)).build()
   }
 
   @GET
   @Path("{authorId}")
   fun getAuthor(@PathParam("authorId") authorId: Int): Response {
     val author = service.getAuthor(authorId) ?: return Response.status(Response.Status.NOT_FOUND).build()
+
     return Response.ok(author).build()
   }
 
@@ -42,17 +44,23 @@ class AuthorsResource {
   @PUT
   @Path("{authorId}")
   @Consumes(MediaType.APPLICATION_JSON)
-  fun updateAuthor(@PathParam("authorId") authorId: Int, @RequestBody author: Author) {
+  fun updateAuthor(@PathParam("authorId") authorId: Int, @RequestBody author: Author): Response {
     if (authorId != author.id) {
-      throw IllegalArgumentException("Author ID from path does not match with author ID from JSON body.")
+      return Response.status(BAD_REQUEST)
+        .entity("Author ID from path does not match with author ID from JSON body.").build()
     }
+
     service.updateAuthor(author)
+
+    return Response.noContent().build()
   }
 
   @PATCH
   @Path("fragile")
+  @Produces(MediaType.TEXT_PLAIN)
   fun fragileOperation(): Response {
     val author = service.fragileOperation()
+
     return Response.ok("Successfully added ${author.firstName} ${author.lastName}").build()
   }
 
