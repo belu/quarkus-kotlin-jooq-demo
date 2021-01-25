4643a0a4f820a6171df93db0730ea927096482c6
Bernhard Lutzmann <bernhard.lutzmann@gmail.com>
2021-01-25 18:06:15 +0100

    Add jOOQ dialect config

--- src/main/kotlin/me/belu/repo/JooqContextProducer.kt
+++ src/main/kotlin/me/belu/repo/JooqContextProducer.kt
@@ -1,6 +1,8 @@
 package me.belu.repo
 
 import io.agroal.api.AgroalDataSource
+import org.eclipse.microprofile.config.inject.ConfigProperty
+import org.jboss.logging.Logger
 import org.jooq.DSLContext
 import org.jooq.SQLDialect
 import org.jooq.impl.DSL
@@ -9,8 +11,23 @@ import javax.enterprise.inject.Produces
 import javax.inject.Inject
 
 @Dependent
-class JooqContextProducer @Inject constructor(private val dataSource: AgroalDataSource) {
+class JooqContextProducer {
+
+  @Inject
+  lateinit var log: Logger
+
+  @Inject
+  lateinit var dataSource: AgroalDataSource
+
+  @ConfigProperty(name = "quarkus.jooq.dialect", defaultValue = "DEFAULT")
+  lateinit var confDialect: String
 
   @Produces
-  fun dslContext(): DSLContext = DSL.using(dataSource, SQLDialect.POSTGRES)
+  fun dslContext(): DSLContext {
+    val dialect = SQLDialect.valueOf(confDialect)
+
+    log.info("jOOQ using dialect: $dialect")
+
+    return DSL.using(dataSource, dialect)
+  }
 }
